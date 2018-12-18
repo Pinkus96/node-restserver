003f478a287d76f9e82cb9515a837e64c6ff5fd5
Luis Castillo <zelda_10a@hotmail.com>
2018-12-18 12:15:14 -0600

    RestServer - usuario2

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require("./config/config");
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -10,28 +13,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
-app.post('/usuario', function(req, res) {
-    let body = req.body;
+app.use(require("./routes/usuario"));
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        });
-    } else {
-        res.json({ body });
-    }
 
+mongoose.connect(process.env.URLDB, (err, res) => {
+    if (err) throw err;
+    console.log("Base de datos ONLINE");
 });
-app.put('/usuario/:id', function(req, res) {
-    let id = req.param.id;
-    res.json({ id });
-});
-app.delete('/usuario', function(req, res) {
-    res.json("delete Usuaios");
-});
-
 app.listen(process.env.PORT, () => console.log("Escuchando el puerto: 3000"));
